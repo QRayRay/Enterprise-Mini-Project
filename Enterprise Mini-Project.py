d5d121b06a36adc415879045750ae5d0a052e344
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-10-31 23:52:34 -0500

    Add files via upload

---
+++ Enterprise Mini-Project.py
@@ -0,0 +1,12 @@
+x = input('enter a number: ')
+y = int(x) + 1
+def myfunction(x):
+    while float(x) <=100:
+        float(x) * 0
+        y = int(x) - 1
+        print('Hello Student')
+        return x
+    myfunction(x)
+
+    
+    
